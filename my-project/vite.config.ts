da71dfaa00bdb1097edb0de2f8ce575b833d5b0c
sarahp <sarahs-pereira@outlook.com>
2025-05-28 19:43:14 -0300

    instala vite e vue-tsc como dependência

--- my-project/vite.config.ts
+++ my-project/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue()],
+  plugins: [vue()],
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
